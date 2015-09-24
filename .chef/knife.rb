62a8ccd09cf402f2f3d2f52927b3de94f764793c
Fiona Sanggang <fsanggang@gmail.com>
2015-09-25 10:23:22 +1200

    Modify knife.rb as necessary

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,8 +1,12 @@
+current_dir = File.dirname(__FILE__)
+
 log_level                :info
 log_location             STDOUT
 node_name                'admin'
-client_key               '/Users/fionasanggang/Codes/chef/chef_training/chef-stack/.chef/admin.pem'
+client_key               '#{current_dir}/../admin.pem'
 validation_client_name   'chef-stack-demo'
-validation_key           '/Users/fionasanggang/Codes/chef/chef_training/chef-stack/dator.pem'
-chef_server_url          'https://10.0.10.2:443'
+validation_key           '#{current_dir}/../validator.pem'
+chef_server_url          'https://10.0.10.2:443/organizations/chef-stack-demo'
 syntax_check_cache_path  '/Users/fionasanggang/Codes/chef/chef_training/chef-stack/.chef/syntax_check_cache'
+
+ssl_verify_mode           :verify_none
