1a9805c618143c89c4f572b6f12b40992ea87048
Fiona Sanggang <fsanggang@gmail.com>
2015-09-25 10:31:25 +1200

    Use " in knife.rb so string interpolation works

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -2,11 +2,11 @@ current_dir = File.dirname(__FILE__)
 
 log_level                :info
 log_location             STDOUT
-node_name                'admin'
-client_key               '#{current_dir}/../admin.pem'
-validation_client_name   'chef-stack-demo'
-validation_key           '#{current_dir}/../validator.pem'
-chef_server_url          'https://10.0.10.2:443/organizations/chef-stack-demo'
-syntax_check_cache_path  '/Users/fionasanggang/Codes/chef/chef_training/chef-stack/.chef/syntax_check_cache'
+node_name                "admin"
+client_key               "#{current_dir}/../admin.pem"
+validation_client_name   "chef-stack-demo"
+validation_key           "#{current_dir}/../validator.pem"
+chef_server_url          "https://10.0.10.2:443/organizations/chef-stack-demo"
+syntax_check_cache_path  "/Users/fionasanggang/Codes/chef/chef_training/chef-stack/.chef/syntax_check_cache"
 
 ssl_verify_mode           :verify_none
