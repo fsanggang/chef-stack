43db7ee1da48508ddecaf4573ef4276b622e7974
Fiona Sanggang <fsanggang@gmail.com>
2015-09-25 10:12:47 +1200

    Generated knife.rb
    fionasanggang@FionaSG chef-stack (master) $ knife configure
    WARNING: No knife configuration file found
    Where should I put the config file? [/Users/fionasanggang/.chef/knife.rb] /Users/fionasanggang/Codes/chef/chef_training/chef-stack/.chef/knife.rb
    Please enter the chef server URL: [https://FionaSG:443] https://10.0.10.2:443
    Please enter an existing username or clientname for the API: [fionasanggang] admin
    Please enter the validation clientname: [chef-validator] chef-stack-demo
    Please enter the location of the validation key: [/etc/chef-server/chef-validator.pem] /Users/fionasanggang/Codes/chef/chef_training/chef-stack/validator.pem
    Please enter the path to a chef repository (or leave blank):
    *****
    
    You must place your client key in:
      /Users/fionasanggang/Codes/chef/chef_training/chef-stack/.chef/admin.pem
    Before running commands with Knife!
    
    *****
    
    You must place your validation key in:
      /Users/fionasanggang/Codes/chef/chef_training/chef-stack/dator.pem
    Before generating instance data with Knife!
    
    *****
    Configuration file written to /Users/fionasanggang/Codes/chef/chef_training/chef-stack/.chef/knife.rb

---
+++ .chef/knife.rb
@@ -0,0 +1,8 @@
+log_level                :info
+log_location             STDOUT
+node_name                'admin'
+client_key               '/Users/fionasanggang/Codes/chef/chef_training/chef-stack/.chef/admin.pem'
+validation_client_name   'chef-stack-demo'
+validation_key           '/Users/fionasanggang/Codes/chef/chef_training/chef-stack/dator.pem'
+chef_server_url          'https://10.0.10.2:443'
+syntax_check_cache_path  '/Users/fionasanggang/Codes/chef/chef_training/chef-stack/.chef/syntax_check_cache'
