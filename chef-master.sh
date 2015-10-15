4d3eb08e4496d195a910cdbe96be8d61d01236d9
Fiona Sanggang <fsanggang@gmail.com>
2015-10-16 09:51:36 +1300

    Check for presence of pivotal.rb before (re)running chef-server-ctl reconfigure

--- chef-master.sh
+++ chef-master.sh
@@ -2,7 +2,7 @@ if ! dpkg --list chef-server-core > /dev/null 2>&1; then
   sudo dpkg -i /vagrant/chef-server-core_12.2.0-1_amd64.deb;
 fi
 
-if chef-server-ctl status | awk '{print $1}' | grep run -q; then
+if ! [ -e /etc/opscode/pivotal.rb ]; then
   chef-server-ctl reconfigure
 fi
 
